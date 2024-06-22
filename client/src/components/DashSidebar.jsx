3fb9979e51b28f5cb90607d5685c0d31678d188e
Daksh <dakshkundu30102002@gmail.com>
2024-06-23 03:53:54 +0530

    added the sign out functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -2,11 +2,15 @@ import { Sidebar } from 'flowbite-react';
 import React from 'react';
 import {HiArrowSmRight, HiUser} from 'react-icons/hi';
 import { useEffect , useState  } from 'react';
-import {Link, useLocation} from 'react-router-dom';
+import {Link, useLocation , useNavigate} from 'react-router-dom';
+import {signoutSuccess} from '../redux/user/userSlice' ;
+import {useDispatch} from 'react-redux' ;
 
 
 export default function DashSidebar() {
     const location = useLocation() ;
+    const navigate = useNavigate();
+    const dispatch = useDispatch() ;
     const [tab , setTab] = useState('') ;
     useEffect(() => {
       const urlParams = new URLSearchParams(location.search)
@@ -15,6 +19,24 @@ export default function DashSidebar() {
         setTab(tabFromUrl) ;
       }
     },[location.search]);
+    const handleSignout = async () => {
+      try{
+          const res = await fetch('/api/user/signout' , {
+              method : 'POST'
+          }) ;
+          const data = await res.json() ;
+          if(!res.ok){
+              console.log(data.message) ;
+          }
+          else{
+              dispatch(signoutSuccess()) ;
+              navigate('/sign-in');
+          }
+      }
+      catch(error){
+          console.log(error.message) ;
+      }
+  } ;
   return (
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
@@ -24,7 +46,7 @@ export default function DashSidebar() {
                         Profile
                     </Sidebar.Item>
                 </Link>
-                <Sidebar.Item icon={HiArrowSmRight} className = 'cursor-pointer'>
+                <Sidebar.Item icon={HiArrowSmRight} className = 'cursor-pointer' onClick = {handleSignout}>
                     Sign Out
                 </Sidebar.Item>
             </Sidebar.ItemGroup>
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -50,6 +50,11 @@ const userSlice = createSlice({
             state.loading = false ;
             state.error = action.payload ;
 
+        } ,
+        signoutSuccess : (state) => {
+            state.currentUser = null ;
+            state.error = null ;
+            state.loading = false ;
         }
 
     },
@@ -63,6 +68,8 @@ export const { signInStart ,
                updateFailure , 
                deleteUserStart , 
                deleteUserSuccess , 
-               deleteUserFailure } = userSlice.actions;
+               deleteUserFailure ,
+               signoutSuccess
+            } = userSlice.actions;
 
 export default userSlice.reducer;
