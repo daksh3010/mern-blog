0de8ac27c44f302f51e21a1e626a9595c8b3953f
Daksh <dakshkundu30102002@gmail.com>
2024-07-05 00:56:19 +0530

    added the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div >
+          <h1 className='text-3xl font-semibold text-center my-7'>About Creative Blogs</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6 p-8">
+            <p>
+            Welcome to <b>Creative Blogs</b> your ultimate destination for a wide array of insightful and captivating articles designed to spark curiosity and ignite passion. At Creative Blogs, we are dedicated to providing our readers with meticulously curated content across a variety of intriguing subjects. Whether you are fascinated by the mysteries of space, the vibrant world of anime, or the cinematic marvels of the movie industry, you will find engaging and thought-provoking articles here.
+            </p>
+<p>Our finance section offers in-depth analyses and expert insights to help you navigate the complex world of economics and investments. For those bitten by the travel bug, our tourism guides provide comprehensive information on exotic destinations and travel tips to enhance your adventures. Tech enthusiasts will find our technology articles brimming with the latest innovations and trends that shape our digital world.</p>
+           
+<p>We are also passionate about education, offering valuable resources and perspectives to support lifelong learning and personal growth. Additionally, our fiction section is a haven for literary enthusiasts, featuring a rich tapestry of stories that transport readers to imaginative worlds.</p>
+            
+<p>At Creative Blogs, we strive to be a beacon of knowledge and entertainment, catering to the diverse interests of our readers. Join us on this journey of discovery, where every article is crafted to inform, inspire, and entertain. Dive into the world of Creative Blogs and let your curiosity lead the way!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
